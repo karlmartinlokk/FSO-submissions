749eaa16bca8f8dd4c6f676385ee063de56cd2b0
Karl-Martin <karlmartin.lokk@gmail.com>
2026-07-21 14:49:12 +0300

    completed ex 1.6: unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Header = ({text}) => <h1> {text} </h1>
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
+
+const StatsDisplay = ({text, rating}) => {
+  return (
+    <div>
+      <p>{text}: {rating}</p>
+    </div>
+  )
+
+}
+
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Display counter={counter}/>
+      <Header text="Give Feedback"></Header>
 
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      <Button onClick={() => setGood(good+1)} text="Good"></Button>
+      <Button onClick={() => setNeutral(neutral+1)} text="Neutral"></Button>
+      <Button onClick={() => setBad(bad+1)} text="Bad"></Button>
+
+      <Header text="Statistics"></Header>
+      <StatsDisplay text="Good" rating={good}></StatsDisplay>
+      <StatsDisplay text="Neutral" rating={neutral}></StatsDisplay>
+      <StatsDisplay text="Bad" rating={bad}></StatsDisplay>
+
     </div>
   )
 }
